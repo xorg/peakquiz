7941a16ca10f2f68ec22c9a4d54a58cda7f6abf5
Stefan Schneider <stefan.schneider@wilmaa.com>
2025-07-19 15:09:10 +0200

    Add simple frontend with react

---
+++ frontend/src/components/AnswerTimer/AnswerTimer.jsx
@@ -0,0 +1,45 @@
+import "./AnswerTimer.scss"
+import { useEffect, useState, useRef } from "react";
+
+
+function AnswerTimer({ duration }) {
+    const [counter, setCounter] = useState(0);
+    const [progressLoaded, setProgressLoaded] = useState(0);
+    const intervalRef = useRef();
+
+
+    useEffect(() => {
+        intervalRef.current = setInterval(() => {
+            setCounter((cur) => cur + 1);
+        }, 1000);
+        return () => clearInterval(intervalRef.current)
+    }, []);
+
+    useEffect(() => {
+        setProgressLoaded(100* (counter / duration));
+
+        if(counter === duration) {
+              clearInterval(intervalRef.current);
+
+              setTimeout(() => {
+                handleTimeUp();
+              }, 1000);
+        }
+    }, [counter]);
+
+    return <div className="answer-timer-container">
+        <div
+         style={{
+            width: `${progressLoaded}%`,
+            backgroundColor: `${
+                progressLoaded < 50 ? `lightgreen` : progressLoaded < 70
+            }`
+         }}
+         className="progress">
+
+        </div>
+    </div>
+
+}
+
+export default AnswerTimer;
---
+++ frontend/src/constants.js
@@ -0,0 +1,57 @@
+export const jsQuizz = {
+  questions: [
+    {
+      question:
+        "https://upload.wikimedia.org/wikipedia/commons/9/91/Piz_Bernina_Aug_2008_close.jpg",
+      choices: [
+        "Piz Palü",
+        "Piz Bernina",
+        "Dufourspitze",
+        "Weisshorn",
+      ],
+      type: "MCQs",
+      correctAnswer: "Piz Bernina",
+    },
+    {
+      question: "https://upload.wikimedia.org/wikipedia/commons/4/43/Weisshorn_2.jpg",
+      choices: [
+        "Dent Blanche",
+        "Dent d'Herèns",
+        "Matterhorn",
+        "Weisshorn",
+      ],
+      type: "MCQs",
+      correctAnswer: "Weisshorn",
+    },
+    {
+      question:
+        "https://upload.wikimedia.org/wikipedia/commons/0/0e/Engstligenalp_und_Wildstrubel.jpg",
+      choices: ["Wildhorn", "Wildstrubel", "Dents du Midi", "Titlis"],
+      type: "MCQs",
+      correctAnswer: "Wildstrubel",
+    },
+    {
+      question: "https://upload.wikimedia.org/wikipedia/commons/e/e5/S%C3%A4ntis_-_Batzberg_IMG_9803.JPG",
+      choices: ["Säntis", "Rigi", "Stanserhorn", "Titlis"],
+      type: "MCQs",
+      correctAnswer: "Säntis",
+    },
+    {
+      question: "https://upload.wikimedia.org/wikipedia/commons/1/14/Landscape_Arnisee-region.JPG",
+      choices: [
+        "Gross Windgällen",
+        "Uri Rotstock",
+        "Galenstock",
+        "Bristen",
+      ],
+      type: "MCQs",
+      correctAnswer: "Gross Windgällen",
+    },
+  ],
+};
+
+export const resultInitialState = {
+  score: 0,
+  correctAnswers: 0,
+  wrongAnswers: 0
+}
