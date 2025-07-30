ee3e6bb3611ef9c4b3da5242848ace937cd0cf4a
Stefan Schneider <stefan.schneider@wilmaa.com>
2025-07-30 22:09:56 +0200

    Add correct allowed hosts

--- peakquiz/settings.py
+++ peakquiz/settings.py
@@ -24,15 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS: list[str] = ["192.168.0.149", "127.0.0.1", "localhost", "192.168.1.139"]
+ALLOWED_HOSTS: list[str] = ["127.0.0.1", "localhost", "xorg112.pythonanywhere.com"]
 
 SECRET_KEY: str | None = os.environ.get("SECRET_KEY")
 
 DEBUG = bool(os.environ.get("DEBUG", default=0))
-
-# 'DJANGO_ALLOWED_HOSTS' should be a single string of hosts with a space between each.
-# For example: 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
-# ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
