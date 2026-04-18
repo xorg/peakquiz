c34fba80a4a9fc7a24974d2eca5619904a7a8945
Stefan <xorg112@gmail.com>
2026-04-18 15:57:04 +0200

    Change backend url in github action

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,7 +1,13 @@
 import sys
+from pathlib import Path
+
 sys.path.insert(0, '/home/xorg/peakquiz_v2/backend')
 
-from app.main import app as asgi_app
-from a2wsgi import ASGIMiddleware
+from dotenv import load_dotenv  # noqa: E402
+from a2wsgi import ASGIMiddleware  # noqa: E402
+
+load_dotenv(Path(__file__).parent / '.env')
+
+from app.main import app as asgi_app  # noqa: E402
 
 application = ASGIMiddleware(asgi_app)
