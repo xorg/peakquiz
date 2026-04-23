49f737989d729787e97735cf540219ee9119935f
Stefan <xorg112@gmail.com>
2026-04-23 11:32:52 +0200

    Fix frontend build error

--- frontend/src/utils/cloudinary.ts
+++ frontend/src/utils/cloudinary.ts
@@ -1,4 +1,4 @@
 export function cdnUrl(url: string, width: number): string {
   if (!url) return url
-  return url.replace('/upload/', `/upload/w_${width},c_limit,f_auto,q_auto/`, 1)
+  return url.replace('/upload/', `/upload/w_${width},c_limit,f_auto,q_auto/`)
 }
